d472b28d3f0e9218516da465b7fb882cd2f11b37
Steven Edouard <steven.edouard1@gmail.com>
2021-03-20 15:58:54 -0400

    Update ps-deploy.ps1
    Remove multi-line string

--- ps-deploy.ps1
+++ ps-deploy.ps1
@@ -8,8 +8,5 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 $key = [string]$apiKey
 $secret = [string]$apiSecret
 $name = [string]$vmName
-(choco install vmpower-agent --source "'.;https://chocolatey.org/api/v2/'" -params `
-"/API_KEY:'$key' `
-/API_SECRET:'$secret' `
-/VM_ID:'$name'")
+choco install vmpower-agent -y --source "'.;https://chocolatey.org/api/v2/'" -params ` "/API_KEY:'$key' /API_SECRET:'$secret' /VM_ID:'$name'"
   
