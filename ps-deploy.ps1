7c3a637ed60235ffa48b54945cd40e51ef7ba4fd
Steven Edouard <steven.edouard1@gmail.com>
2021-03-20 15:47:42 -0400

    Update ps-deploy.ps1
    Remove --force flag

--- ps-deploy.ps1
+++ ps-deploy.ps1
@@ -8,7 +8,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 $key = [string]$apiKey
 $secret = [string]$apiSecret
 $name = [string]$vmName
-(choco install vmpower-agent --source "'.;https://chocolatey.org/api/v2/'" --force -params `
+(choco install vmpower-agent --source "'.;https://chocolatey.org/api/v2/'" -params `
 "/API_KEY:'$key' `
 /API_SECRET:'$secret' `
 /VM_ID:'$name'")
